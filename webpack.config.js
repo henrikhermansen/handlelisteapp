ae6059045871e1c789893edc12d4333bb729b702
Henrik Hermansen <henrikhermansen@bekk.no>
2018-01-15 23:32:09 +0100

    Fix building css from less. Add css to index.html.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: [
+    './src/index.jsx',
+    './src/app.less',
+  ],
   output: {
     path: path.resolve(__dirname, 'public/build'),
     filename: 'app.js',
@@ -15,11 +19,20 @@ module.exports = {
       },
       {
         test: /\.less$/,
-        use: ['css-loader', 'less-loader'],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader'],
+        }),
       },
     ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      allChunks: true,
+    }),
+  ],
 };
