8393803bbff4a2afd08d3635151e560c055b94ed
Henrik Hermansen <henrikhermansen@bekk.no>
2018-01-21 13:02:06 +0100

    Adding hot module reloading, moved the pre-commit hook to where it should be (not in scripts) and fixed lint paths to include js-files directly in the src folder. It should be noted that the webpack config has developed into a dev environment config, and should not be used in production.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,29 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
 
 import App from './app';
 import rootReducer from './reducers';
 
 const store = createStore(rootReducer);
 
-render(
-  (
+const renderApp = Component => render(
+  <BrowserRouter>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AppContainer>
+        <Component />
+      </AppContainer>
     </Provider>
-  )
+  </BrowserRouter>
   , document.querySelector('#main'),
 );
+
+renderApp(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    render(App);
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    'react-hot-loader/babel',
+    'react-hot-loader/patch',
+    './src/index.js',
+  ],
   output: {
     path: path.resolve(__dirname, 'public/build'),
+    publicPath: 'build/',
     filename: 'app.js',
   },
   module: {
     rules: [
       {
         test: /.jsx?$/,
-        use: 'babel-loader',
+        loader: 'babel-loader',
         exclude: /node_modules/,
+        options: {
+          cacheDirectory: true,
+          plugins: [
+            'react-hot-loader/babel',
+          ],
+        },
       },
       {
         test: /\.less$/,
@@ -31,5 +45,13 @@ module.exports = {
       filename: 'app.css',
       allChunks: true,
     }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true,
+    historyApiFallback: true,
+  },
+  devtool: 'inline-source-map',
 };
