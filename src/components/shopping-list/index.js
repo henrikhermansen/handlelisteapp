bf803eb811664c455055054a93fa393a0283f079
Henrik Hermansen <henrikhermansen@bekk.no>
2018-02-20 00:45:07 +0100

    Map and sort data for the shopping list.

--- src/components/shopping-list/index.js
+++ src/components/shopping-list/index.js
@@ -2,9 +2,19 @@ import { connect } from 'react-redux';
 
 import ShoppingList from './shopping-list';
 
+const findItemFromId = (items, id) => items.find(item => item.id === id);
+
+const mapDataToShoppingList = (items, bagItems) => bagItems.map(bagItem => ({
+  ...bagItem,
+  added: new Date(bagItem.added),
+  name: findItemFromId(items, bagItem.itemId).name,
+}));
+
+const sortByDateAdded = (a, b) => a.added - b.added;
+
 const mapStateToProps = state => ({
   isLoading: state.bagItems.isLoading,
-  items: state.bagItems.items,
+  items: mapDataToShoppingList(state.items.items, state.bagItems.items).sort(sortByDateAdded),
 });
 
 export default connect(mapStateToProps)(ShoppingList);
