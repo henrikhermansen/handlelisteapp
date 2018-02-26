5112014c7daac9897ace05d18c1e595733e02733
Henrik Hermansen <henrikhermansen@bekk.no>
2018-02-26 23:11:14 +0100

    Begin work on AddBagItem component
    Please note this component is WIP.

--- src/components/add-bag-item/add-bag-item.js
+++ src/components/add-bag-item/add-bag-item.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const AddBagItem = () => <div />;
+import IsLoadingWrapper from '../is-loading-wrapper/is-loading-wrapper';
+
+class AddBagItem extends Component {
+  static propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+    items: PropTypes.array.isRequired,
+    addBagItem: PropTypes.func.isRequired,
+  };
+
+  state = {
+    isLoading: false,
+  };
+
+  render() {
+    return (
+      <IsLoadingWrapper isLoading={this.props.isLoading || this.state.isLoading}>
+        <div>{ this.props.items }</div>
+        <button onClick={this.props.addBagItem}>Dummy</button>
+      </IsLoadingWrapper>
+    );
+  }
+}
 
 export default AddBagItem;
