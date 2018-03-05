909a212d52fac7add5894b615b2ced2e991a130a
Henrik Hermansen <henrikhermansen@bekk.no>
2018-03-05 22:56:17 +0100

    Make navigation toggleable
    This commit makes the navigation section's visibility toggleable through
    a toggle button (hamburger icon) at the bottom of the page.

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import classnames from 'classnames';
 
 import NavigationItem from './navigation-item';
 import * as paths from '../../constants/paths';
+import KeyCodes from '../../constants/key-codes';
 
 import './navigation.less';
 
-const Navigation = () => (
-  <div className="fixed-bottom">
-    <nav className="navigation">
-      <NavigationItem to={paths.shopping} exact>Handleliste</NavigationItem>
-      <NavigationItem to={paths.items} exact>Vareutvalg</NavigationItem>
-    </nav>
-  </div>
-);
+class Navigation extends PureComponent {
+  state = { visible: false };
+
+  onKeyDown = (event) => {
+    if (event.which === KeyCodes.ENTER) {
+      this.toggleVisible();
+    }
+  };
+
+  toggleVisible = () => this.setState({ visible: !this.state.visible });
+
+  render() {
+    const { visible } = this.state;
+
+    return (
+      <div className="fixed-bottom">
+        <div className="navigation-wrapper">
+          <div
+            className={classnames(
+              'navigation-toggle',
+              { 'navigation-toggle--active': visible },
+            )}
+            onClick={this.toggleVisible}
+            onKeyDown={this.onKeyDown}
+            role="button"
+            aria-pressed={String(visible)}
+            tabIndex="0"
+          >
+            <span />
+            <span />
+            <span />
+          </div>
+          <nav
+            className={classnames(
+              'navigation',
+              { 'navigation--visible': visible },
+            )}
+          >
+            <NavigationItem to={paths.shopping} exact>Handleliste</NavigationItem>
+            <NavigationItem to={paths.items}>Vareutvalg</NavigationItem>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Navigation;
