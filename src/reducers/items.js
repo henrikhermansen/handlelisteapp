099eed07af9bf8642b9a0805218ca2a638ce0f66
Henrik Hermansen <henrikhermansen@bekk.no>
2018-03-09 18:55:04 +0100

    Add reducer handling for update and add item

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,4 +1,10 @@
-import { FETCH_ITEMS, FETCH_ITEMS_FAILED, FETCH_ITEMS_SUCCESS } from '../actions/items';
+import {
+  FETCH_ITEMS,
+  FETCH_ITEMS_FAILED,
+  FETCH_ITEMS_SUCCESS,
+  UPDATE_ITEM,
+  ADD_ITEM,
+} from '../actions/items';
 
 const defaultState = {
   items: [],
@@ -18,6 +24,16 @@ export default (state = defaultState, action) => {
     case FETCH_ITEMS_FAILED:
       return {
         ...state, isLoading: false, isFetched: true, error: action.error,
+      };
+    case UPDATE_ITEM:
+      return {
+        ...state,
+        items: state.items.map(item => (item.id === action.item.id ? action.item : item)),
+      };
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [...state.items, action.item],
       };
     default:
       return state;
