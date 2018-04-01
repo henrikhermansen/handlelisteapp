81f69aa4bc73878ba2e8bf4b91de231c25e91fbb
Henrik Hermansen <henrikhermansen@bekk.no>
2018-04-01 18:47:51 +0200

    Transition from webpack to parcel
    This commit fully transitions the build of the project from webpack to
    parcel. This simplifies the build and reduces build time.
    
    This solves #8
    
    Browserconfig and mstile files are deleted, as I decided not to care about
    Windows Phone.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
+/* eslint-disable import/first */
+import './vendor';
+
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { AppContainer } from 'react-hot-loader';
 
 import App from './containers/app';
 import ErrorBoundary from './components/error-boundary/error-boundary';
 
-const renderApp = Component => render(
+render(
   <BrowserRouter>
-    <AppContainer>
-      <ErrorBoundary>
-        <Component />
-      </ErrorBoundary>
-    </AppContainer>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </BrowserRouter>
   , document.querySelector('#app'),
 );
-
-renderApp(App);
-
-// Webpack Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./containers/app', () => {
-    render(App);
-  });
-}
---
+++ src/vendor.js
@@ -0,0 +1,7 @@
+import('babel-polyfill');
+import('classnames');
+import('firebase');
+import('prop-types');
+import('react');
+import('react-dom');
+import('react-router-dom');
