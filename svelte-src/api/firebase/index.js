b97509012c9b69a6ce1bdbc4e6ec31206ff92bfa
Henrik Hermansen <henrikhermansen@bekk.no>
2019-07-26 21:06:49 +0200

    Add remove functionality to selection

--- svelte-src/api/firebase/index.js
+++ svelte-src/api/firebase/index.js
@@ -2,3 +2,4 @@ export { default as createFirebaseStore } from './createFirebaseStore';
 export { BAG_ITEMS, ITEMS, CONNECTED } from './refs';
 export { default as add } from './add';
 export { default as update } from './update';
+export { default as remove } from './remove';
---
+++ svelte-src/api/firebase/remove.js
@@ -0,0 +1,7 @@
+import database from './database';
+
+const remove = async (ref, key) => {
+  await database.ref(`${ref}/${key}`).remove();
+};
+
+export default remove;
